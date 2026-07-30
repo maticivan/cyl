3d515179a11fcf27cfde2aca42c4ccda82f22315
maticivan <maticivan@gmail.com>
2026-07-30 10:44:00 -0400

    Update 2.0.0

--- cylinders.cpp
+++ cylinders.cpp
@@ -22,12 +22,14 @@
 
 #include "src/fundamental/SHF_standardHeadersMinimal.cpp"
 #include <thread>
-#include "src/fundamental/ssm.cpp" 
+#include "src/fundamental/ssm.cpp"
+#include "src/fundamental/fst.cpp" 
 #include "src/fundamental/MFRF_massiveFindReplace.cpp"
 #include "src/fundamental/BF_bijFunction.cpp"
 #include "src/fundamental/TMF_timer.cpp"
 #include "src/fundamental/SF_strings.cpp"
 #include "src/fundamental/GF_globalInitsMinimal.cpp"
+#include "src/fundamental/RNDF_randomCodesAndPermutations.cpp"
 #include "src/fundamental/IOF_io.cpp"
 #include "src/tools/CLAT_commandLineArguments.cpp"
 #include "src/algebra/FA_fractions.cpp"
@@ -35,9 +37,10 @@
 #include "src/algebra/PA_polynomials.cpp"
 #include "src/cylinders/CPC_cPolynomials.cpp"
 #include "src/cylinders/CC_cylinders.cpp"
-
-
+ 
 int main(int argc, char **argv){
+    TMF::Timer tm;
+    tm.start();
     CLAT::CommandLineArgs inputArgs=CLAT::getArguments(CLAT::argsInStrings(argc,argv));
     std::string sourceTxt;
     if(inputArgs.inputFile=="notFound"){
@@ -58,6 +61,8 @@ int main(int argc, char **argv){
     if((FA::GL_OVERFLOW)||(FA::GL_DIVBY0)){
         std::cout<<"WARNING: The program had a division by zero or an overflow in calculation. Results are not trustworthy. \n";
     }
+    tm.end();
+    std::cout<<"Total time: "<<tm.getTimeSec()<<" seconds.\n";
     return 0;
 }
 
